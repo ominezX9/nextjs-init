da315f39e25aa55adbcc7793c1aa00406d3d212f
David Agu <david.agu@iisysgroup.com>
2023-06-22 11:08:25 +0100

    Delete App.css, Rewrite Index.css and Add Tailwind

---
+++ reacts/tailwind-react/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+// import './App.css';
+import HelloWorld from "./Components/HelloWorld";
+
+function App(){
+  return(
+    <div>
+      <Header/>
+      <HelloWorld/>
+    </div>
+  )
+}
+
+export default App;
---
+++ reacts/tailwind-react/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header(){
+    return(
+        <header className="app-header">    
+        TutSpot.io
+        </header>
+    )
+}
+
+export default Header;
